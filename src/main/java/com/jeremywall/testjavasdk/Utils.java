ac2ccf75f5695d86d2dab50a3feccaf59157a72d
Jeremy Wall <jeremywall@users.noreply.github.com>
2021-10-14 22:57:19 -0700

    init

---
+++ src/main/java/com/jeremywall/testjavasdk/Utils.java
@@ -0,0 +1,12 @@
+package com.jeremywall.testjavasdk;
+
+public class Utils {
+
+    public static String getGreeting(String name) {
+
+        if (name == null) {
+            return "Hello";
+        }
+        return "Hello " + name;
+    }
+}
