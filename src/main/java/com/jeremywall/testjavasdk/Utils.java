f9142a86f307e9bef985427c20c53b8480ee0d86
Jeremy Wall <jeremywall@users.noreply.github.com>
2021-10-15 12:36:46 -0700

    test formatter

--- src/main/java/com/jeremywall/testjavasdk/Utils.java
+++ src/main/java/com/jeremywall/testjavasdk/Utils.java
@@ -1,10 +1,9 @@
 package com.jeremywall.testjavasdk;
-
 public class Utils {
-
-  public static String getGreeting(String name) {
-
-    if (name == null) {
+  public static String getGreeting(String name)
+  {
+    if (name == null)
+    {
       return "Hello";
     }
     return "Hello " + name;
