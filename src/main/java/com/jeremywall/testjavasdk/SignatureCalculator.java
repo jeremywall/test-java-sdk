f8e4c4ed702e1ed360003f8278cfae41f89e6b27
Jeremy Wall <jeremywall@users.noreply.github.com>
2021-10-19 15:52:48 -0700

    Create SignatureCalculator.java

---
+++ src/main/java/com/jeremywall/testjavasdk/SignatureCalculator.java
@@ -0,0 +1,18 @@
+package com.jeremywall.testjavsdk;
+
+public class SignatureCalculator {
+
+    private enum ApiCall {
+        STATIONS,
+        NODES,
+        SENSORS,
+        SENSOR_ACTIVITY,
+        SENSOR_CATALOG,
+        ;
+    }
+
+    public String calculateStationsSignature(String apiKey, String apiSecret, long apiRequestTimestamp) {
+
+return "";
+    }
+}
